09aebf0cd85787d54f7ecd92141aafbecfabacdb
Rodrigo Lopes <52975315+rodrigoczlopes@users.noreply.github.com>
2020-04-13 23:13:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("That's All Folks");
+
+var nomeDaVariavel = "valor da variável";
+var outraVariável = 'valor da outra variável';
+var variavelIdade = 23;
+var variavelTenhoCabeloPreto = true;
+var variavelIndefinida;
+
+console.log(nomeDaVariavel);
+console.log(outraVariável);
+console.log(variavelIdade);
+console.log(variavelTenhoCabeloPreto);
+console.log(variavelIndefinida);
